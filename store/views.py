a63d8c315a91d9b82141f848ab879ca09e4a64cc
Siva Jr <sivarohit36@gmail.com>
2023-10-27 21:59:20 +0530

    added android apps template

--- store/views.py
+++ store/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
+def mobileApps(request):
+    return render(request, 'android/home.html')
+
 def mobileAppDetail(request):
-    return render(request, 'mobile-apps/app_detail.html')
+    return render(request, 'android/app_detail.html')
